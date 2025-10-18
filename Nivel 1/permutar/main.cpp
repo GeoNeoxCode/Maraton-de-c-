a61c7e5068f60046b88904edafbc355f24ee5b1e
erickmacias08 <emacias@alumno.montepiedra.edu.ec>
2025-10-18 11:56:53 -0500

    Add files via upload

---
+++ Nivel 1/permutar/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void permutar(string s,int l,int r){
+    if(l==r) cout<<s<<endl;
+    else{
+        for(int i=l;i<=r;i++){
+            swap(s[l],s[i]);
+            permutar(s,l+1,r);
+            swap(s[l],s[i]);
+        }
+    }
+}
+
+int main(){
+    string str="ABC";
+    permutar(str,0,str.size()-1);
+}
